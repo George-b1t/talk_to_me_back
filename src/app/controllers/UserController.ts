b29d8944894a2360b9a4c1d5d4c6ad0d43d2e2b3
George-b1t <george.soares509@gmail.com>
2022-08-23 21:30:14 -0300

    add - list user by room

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -22,6 +22,36 @@ class UserController {
       message: "User created successfully",
     });
   }
+
+  async listByRoom(req: Request, res: Response): Promise<Response> {
+    const { room_id } = req.query;
+
+    if (!room_id) {
+      throw new Error("Invalid room");
+    }
+
+    const links = await prismaClient.linkUserRoom.findMany({
+      where: {
+        room_id: Number(room_id),
+      },
+    });
+
+    const users = await prismaClient.user.findMany({
+      where: {
+        id: {
+          in: links.map((link) => link.user_id),
+        },
+      },
+    });
+
+    return res.json({
+      users: users.map((user) => ({
+        id: user.id,
+        name: user.name,
+        nickname: user.nickname,
+      })),
+    });
+  }
 }
 
 export const userController = new UserController();
--- src/app/routes/user.routes.ts
+++ src/app/routes/user.routes.ts
@@ -4,5 +4,6 @@ import { userController } from "../controllers/UserController";
 const userRoutes = Router();
 
 userRoutes.post("/create", userController.create);
+userRoutes.get("/listByRoom", userController.listByRoom);
 
 export { userRoutes };
